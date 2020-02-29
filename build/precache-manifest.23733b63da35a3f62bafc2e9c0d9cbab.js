9a2d7b31892737d5373095cd6dc2ee970add88a5
obada almatrami <obada.matrami92@gmail.com>
2020-02-29 17:05:34 +0200

    finish the routing

---
+++ build/precache-manifest.23733b63da35a3f62bafc2e9c0d9cbab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dce44f04a285135f9e9d99ae35951bc5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0de8947fc2dea08d573c",
+    "url": "/static/css/main.9ac30530.chunk.css"
+  },
+  {
+    "revision": "48dfecace691f13205eb",
+    "url": "/static/js/2.b6bb54dc.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.b6bb54dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0de8947fc2dea08d573c",
+    "url": "/static/js/main.691cc03c.chunk.js"
+  },
+  {
+    "revision": "41bacbbd8fcde4686650",
+    "url": "/static/js/runtime-main.7b43ec4a.js"
+  }
+]);
